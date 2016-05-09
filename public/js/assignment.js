4f514dd9f8d4e22ecb2a58d8a546a9b61dff6394
Nghi <thanhnghi94@gmail.com>
2016-05-09 10:47:43 +0200

    Add service to get ajax data

--- public/js/assignment.js
+++ public/js/assignment.js
@@ -2,8 +2,12 @@
 
 var myApp = angular.module("myApp", []);
 
-myApp.controller("myController", function ($scope, $http) {
-    $http.get("http://localhost:3000/data").success(function(data){
+myApp.service("animalData", function ($http) {
+    return $http.get("http://localhost:3000/data");
+});
+
+myApp.controller("myController", function ($scope, animalData) {
+    animalData.success(function (data) {
         $scope.items = data;
     }).error(function(error){
         $scope.error = error;
