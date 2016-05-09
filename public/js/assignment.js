7b2a79276a9e29860dc39cae8b7bd04dd5a7701b
Nghi <thanhnghi94@gmail.com>
2016-05-09 13:16:05 +0200

    Add dependencies to angularAnimate and uiBootstrap modules

--- public/js/assignment.js
+++ public/js/assignment.js
@@ -1,17 +1,17 @@
 // put your javascript code here
 
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ['ngAnimate', 'ui.bootstrap']);
 
 myApp.service("animalData", function ($http) {
     return $http.get("http://localhost:3000/data/AnimalsData.json");
 });
 
 myApp.controller("myController", function ($scope, animalData) {
-    $scope.hide = true;
-
     animalData.success(function (data) {
         $scope.items = data;
     }).error(function(error){
         $scope.error = error;
     });
+
+    $scope.isCollapsed = true;
 });
